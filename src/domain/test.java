a8c3420f56466c1738f5dbdd6c47ec62b3244f7f
sicfran774 <sicfran.774@gmail.com>
2023-05-03 19:50:18 -0700

    Finished Author access stuff, updated SQL script

---
+++ src/domain/test.java
@@ -0,0 +1,10 @@
+package domain;
+
+import persistence.BookDataAccess;
+
+public class test {
+	public static void main(String[] args) {
+		//BookDataAccess.createBook(true, "The Book", "It's about the book", null, 0, 100, null, null, null);
+		System.out.print(BookDataAccess.deleteAuthor(2));
+	}
+}
